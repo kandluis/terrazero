4064efa2bd9079b2d9f01f8cd4e54e59235ad172
Luis Perez <luis.perez.live@gmail.com>
2018-11-16 16:52:07 -0800

    Adding logic to culttrack for moving places
    Former-commit-id: 634213c2d67ab90e717c33f8b2747711d6bcd411

--- code/faction_test.py
+++ code/faction_test.py
@@ -183,6 +183,188 @@ class TestPlayer(unittest.TestCase):
       })
     self.assertEqual(player.coins, 17)
 
+class TestCultBoard(unittest.TestCase):
+  def testInitializationOfEmptyCultBoard(self):
+    cult = CultBoard(factions=[])
+    for _, order in cult.occupied_orders.items():
+      self.assertFalse(order)
+    for _, track in cult.positions.items():
+      self.assertFalse(track)
+
+  def testInitializationSingleFaction(self):
+    halfling = Halflings()
+    cultBoard = CultBoard(factions=[halfling])
+    for _, order in cultBoard.occupied_orders.items():
+      self.assertFalse(order)
+    # Halflings should be on earth and air.
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+      Terrain.PLAIN: 1
+      })
+    self.assertEqual(cultBoard.positions[CultTrack.AIR], {
+      Terrain.PLAIN: 1
+    })
+    self.assertEqual(cultBoard.positions[CultTrack.FIRE], {
+      Terrain.PLAIN: 0
+      })
+    self.assertEqual(cultBoard.positions[CultTrack.WATER], {
+      Terrain.PLAIN: 0
+      })
+
+
+  def testInitializationMultipleFactions(self):
+    
+    halfling = Halflings()
+    engineer = Engineers()
+    cultBoard = CultBoard(factions=[halfling, engineer])
+
+    for _, order in cultBoard.occupied_orders.items():
+      self.assertFalse(order)
+    # Halflings should be on earth and air.
+    # Engineers will be zero everywhere.
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+      Terrain.PLAIN: 1,
+      Terrain.MOUNTAIN: 0
+      })
+    self.assertEqual(cultBoard.positions[CultTrack.AIR], {
+      Terrain.PLAIN: 1,
+      Terrain.MOUNTAIN: 0
+    })
+    self.assertEqual(cultBoard.positions[CultTrack.FIRE], {
+      Terrain.PLAIN: 0,
+      Terrain.MOUNTAIN: 0
+      })
+    self.assertEqual(cultBoard.positions[CultTrack.WATER], {
+      Terrain.PLAIN: 0,
+      Terrain.MOUNTAIN: 0
+      })
+
+  def testSacrificePriestToOrderOvershootPower(self):
+    halfling = Halflings()
+    player = Player(faction=halfling)
+    cultBoard = CultBoard(factions=[halfling])
+
+    # We move 3 spaces and gain 1 power, since we overshoot the 3 token.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (3, 1))
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+        Terrain.PLAIN: 4
+      })
+    # We occupy the first place for the EARTH order.
+    self.assertEqual(cultBoard.occupied_orders[CultTrack.EARTH], {
+      1: Terrain.PLAIN
+      })
+
+  def testSacrificePriestToOrderLandOnPower(self):
+    halfling = Halflings()
+    player = Player(faction=halfling)
+    cultBoard = CultBoard(factions=[halfling])
+
+    # We move 3 spaces and gain 1 power, since we land on 3.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.FIRE),
+      (3, 1))
+    self.assertEqual(cultBoard.positions[CultTrack.FIRE], {
+        Terrain.PLAIN: 3
+      })
+    # We occupy the first place for the MOUNTAIN order.
+    self.assertEqual(cultBoard.occupied_orders[CultTrack.FIRE], {
+      1: Terrain.PLAIN
+      })
+
+  def testSacrificeManyPriestsTownKeys(self):
+    halfling = Halflings()
+    player = Player(faction=halfling)
+    cultBoard = CultBoard(factions=[halfling])
+
+    # We move 3 spaces and gain 1 power, since we land on 4.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (3, 1))
+    # We move another 2 spaces and gain 2 power since we land on 6. 
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (2, 2))
+    # We move another 2 spaces and gain 2 power since we land on 8. 
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (2, 2))
+
+    # We move another 1 spaces and gain 0 power since we can't move past 9.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (1, 0))
+
+    # The cult orders are full by us. 
+    self.assertEqual(cultBoard.occupied_orders[CultTrack.EARTH], {
+      1: Terrain.PLAIN, 2: Terrain.PLAIN, 3: Terrain.PLAIN, 4: Terrain.PLAIN
+      })
+
+    # We can still try to move but fail.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (0, 0))
+
+    # We're at the 9th position.
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+      Terrain.PLAIN: 9
+      })
+
+    # We gain a town key and try to move (only 1) to 10.
+    self.assertIsNone(player.GainTown(TownKey.TODO))
+    # Gain 3 power!
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player, CultTrack.EARTH),
+      (1, 3))
+    # We're occupying the town! Woot!
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+        Terrain.PLAIN: 10
+      })
+
+  def testCantOccupyAlreadyTakenTown(self):
+    factions = [Halflings(), Engineers()]
+    player1 = Player(faction=factions[0])
+    player2 = Player(faction=factions[1])
+    cultBoard = CultBoard(factions=factions)
+
+    # Both players have town keys.
+    self.assertIsNone(player1.GainTown(TownKey.TODO))
+    self.assertIsNone(player2.GainTown(TownKey.TODO))
+
+    # Halfling takes the earth track by 1 -> 4 -> 6 -> 8 -> 10
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player1, CultTrack.EARTH),
+      (3, 1))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player1, CultTrack.EARTH),
+      (2, 2))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player1, CultTrack.EARTH),
+      (2, 2))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player1, CultTrack.EARTH),
+      (2, 3))
+
+    # Now engineer tries by 0 -> 1 -> 2 -> ... -> 9 -> 9 ... -> 9.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,1))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,2))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,2))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (1,0))
+
+    # Try again, but even with the town-key, player will fail.
+    self.assertEqual(cultBoard.SacrificePriestToOrder(player2, CultTrack.EARTH), 
+      (0,0))
+    # Player still has town key.
+    self.assertTrue(player2.UseTownKey())
+
+    # Positions of players. 
+    self.assertEqual(cultBoard.positions[CultTrack.EARTH], {
+      Terrain.PLAIN: 10,
+      Terrain.MOUNTAIN: 9
+      })
+
 
 
 if __name__ == '__main__':
