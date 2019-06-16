8656526a25513f509810fcd6a877417ebd82f8a1
Luis Perez <luis.perez.live@gmail.com>
2019-06-15 23:21:38 -0700

    Updated test script to actually run all tests

--- code/test.sh
+++ code/test.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 BASEDIR=$(dirname "$0")
 
+# Set the python path to contain our directory as a module.
 export PYTHONPATH="${PYTHONPATH}:${BASEDIR}"
+
+# Check type annotations.
 mypy --strict -p simulation
+
+# Run all tests.
+python -m unittest discover --pattern="*_test.py"
