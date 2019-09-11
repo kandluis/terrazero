1e92117925fb143b39601df46ab202d68a4ce57d
Luis Perez <luis.perez.live@gmail.com>
2019-09-11 11:45:34 -0400

    Add more documentation to data.py scraper for snellman site

--- notebook/data.py
+++ notebook/data.py
@@ -19,16 +19,32 @@ from typing import Callable, Dict, List, Optional, Text, Tuple
 _END_OF_SAMPLE_TOKEN = '\n<|endoftext|>\n'
 _WORKER_COUNT = 100
 
+
 class Game:
+  """An in-memory representation of a TM game.
+
+  Properties:
+    game_id: The game id assigned to this game by terra.snellman.net
+    player_count: The number of players who started this game.
+    total_vp: The sum of all player's VP.
+  """
+
   def __init__(self, json) -> None:
-    self.game_id = json['game']
-    self.player_count = json['player_count']
-    self.total_vp = json['events']['faction']['all']['vp']['round']['all']
+    if len(json) == 1:
+      self.game_id = json['game']
+      self.player_count = 0
+      self.total_vp = 0
+    else:
+      self.game_id = json['game']
+      self.player_count = json['player_count']
+      self.total_vp = json['events']['faction']['all']['vp']['round']['all']
 
   def averageVPPerPlayer(self) -> float:
+    """Returns the average VP earned by each player in this game. """
     return self.total_vp / self.player_count
 
   def __str__(self) -> str:
+    """Returns a string representation of a Game."""
     return """
         Game: %s
         Players: %s
@@ -47,17 +63,19 @@ def fetchAllSummaryData(minDate: datetime.datetime,
   """Fetches the Games based on summary data.
 
     Args:
-        minDate: The smallest date from which to fetch games (only the month matters)
-        maxDate: The largest date from which to fetch games (only the month matters)
-        keepPredicate: A callable. For memory efficiency, we can push down filtering on games
-            using this function. If this function returns true, the Game is kept. Otherwise
-            it is immediately discared.
-        maxGames: The maximum number of games to return. Games are retrieved from latest to
-            oldest, date wise.
+        minDate: The smallest date from which to fetch games
+          (only the month matters)
+        maxDate: The largest date from which to fetch games
+          (only the month matters)
+        keepPredicate: A callable. For memory efficiency, we can push down
+          filtering on game using this function. If this function returns true,
+          the Game is kept. Otherwise it is immediately discared.
+        maxGames: The maximum number of games to return. Games are retrieved
+          from latest to oldest, date wise.
 
     Returns:
         A list of Game objects fetched from Terra Snellman.
-    """
+  """
   BASE_URL = "https://terra.snellman.net/data/events"
   results: List[Game] = []
   while minDate < maxDate and (not maxGames or len(results) < maxGames):
@@ -84,7 +102,6 @@ def fetchAllSummaryData(minDate: datetime.datetime,
         maxDate -= relativedelta.relativedelta(months=1)
         continue
 
-    prevNGames = len(results)
     for obj in data:
       game = Game(obj)
       if keepPredicate(game):
@@ -97,16 +114,26 @@ def fetchAllSummaryData(minDate: datetime.datetime,
 
 
 def keepHighScoringGames(game: Game) -> bool:
+  """Filter function that keeps only high scoring games."""
   return game.averageVPPerPlayer() > 120
 
 
 def downloadLogForGame(game: Game) -> Text:
+  """Downloads the entire log (text) for the given game.
+
+  Args:
+    game: The game for which the playlog will be downloaded.
+
+  Returns:
+    A string consisting of all commands in the game log (seperated by ".").
+  """
   kBaseUrl = "https://terra.snellman.net/app/view-game/"
   res = requests.post(kBaseUrl, data={'game': game.game_id})
   data = json.loads(res.content)
   commands = [
-      command['commands'].split(".") for command in data['ledger']
-      if 'commands' in command
+      command.get('commands', '').split(".") or command.get('comment', '')
+      for command in data['ledger']
+      if 'commands' in command or 'comment' in command
   ]
   gameLog = ". ".join(
       item.strip().lower() for sublist in commands for item in sublist)
@@ -114,6 +141,15 @@ def downloadLogForGame(game: Game) -> Text:
 
 
 def parseShardedFilename(filename: Text) -> Tuple[int, int]:
+  """Parses a sharded filename
+
+  Args:
+    filename: Must be in the format
+      /path/to/file-{shardNumber}-of-{totalShards}.suffix
+
+  Returns:
+    A tuple of (shardNumber, totalShards)
+  """
   prefix: Text = filename.split(".")[0]
   parts: List[Text] = prefix.split("-")
   totalShards = int(parts[-1])
@@ -121,7 +157,12 @@ def parseShardedFilename(filename: Text) -> Tuple[int, int]:
   return (shardNumber, totalShards)
 
 
-def loadSentencesFromDisk() -> List[Text]:
+def loadGameLedgesFromDisk() -> List[Text]:
+  """Loads the game ledges which have already been downloaded from disk.
+
+  Returns:
+    A list of game ledges read from the given location.
+  """
   for (dirpath, dirnames, filenames) in os.walk("snellman"):
     maxFileInFileSet: Dict[int, Text] = {}
     for filename in filenames:
@@ -148,11 +189,21 @@ def loadSentencesFromDisk() -> List[Text]:
     return []
 
 
-def fetchAllGameSetences(detailsLocal: bool = False,
-                         summaryLocal: bool = False,
-                         saveEvery: int = 1000,
-                         maxGames: int = 20000) -> Text:
+def fetchAllGameLogs(detailsLocal: bool = False,
+                     summaryLocal: bool = False,
+                     saveEvery: int = 1000,
+                     maxGames: int = 20000) -> Text:
   """Downloads game data and dumps to disk.
+
+  Args:
+    detailsLocal: If true, game data is loaded only from the disk data.
+    summaryLocal: If true, summary data is loaded only from disk.
+    saveEvery: Specifies approximately how many games to process before storing
+        a checkpoint on disk.
+    maxGames: The approximate number of maximum games to download.
+
+  Returns:
+    A single string containing all loaded game logs.
   """
   def downloadGameWrapper(info: Tuple[int, Game]) -> Text:
     i, game = info
@@ -167,16 +218,17 @@ def fetchAllGameSetences(detailsLocal: bool = False,
       keepPredicate=keepHighScoringGames,
       maxGames=maxGames,
       local=summaryLocal)
-  sentences: List[Text] = loadSentencesFromDisk()
+  sentences: List[Text] = loadGameLedgesFromDisk()
   if not detailsLocal:
     try:
-      # Try to continue where we left off. Assume user "did the right thing" and
-      # is using the same "data" to load the games.
+      # Try to continue where we left off. Assume user "did the right thing"
+      # and is using the same "data" to load the games.
       # Migrate to a set of ids eventually.
       processedSinceLastSave = 0
       for i in range(len(sentences), len(data), _WORKER_COUNT):
         with futures.ThreadPoolExecutor(max_workers=_WORKER_COUNT) as executor:
-          inp = [(k, data[k]) for k in range(i, min(i + _WORKER_COUNT, len(data)))]
+          inp = [(k, data[k])
+                 for k in range(i, min(i + _WORKER_COUNT, len(data)))]
           concurrent_sentences = executor.map(downloadGameWrapper, inp)
         sentences.extend(concurrent_sentences)
         processedSinceLastSave += _WORKER_COUNT
@@ -195,6 +247,7 @@ def fetchAllGameSetences(detailsLocal: bool = False,
 
 
 def setUpParser() -> argparse.ArgumentParser:
+  """Setup the parser for command line arguments."""
   parser = argparse.ArgumentParser(description='Download TM game data.')
   parser.add_argument(
       '--saveEvery',
@@ -212,22 +265,31 @@ def setUpParser() -> argparse.ArgumentParser:
       '--summaryLocal', dest='summaryLocal', action='store_true')
   parser.add_argument(
       '--no-summaryLocal', dest='summaryLocal', action='store_false')
-  parser.set_defaults(feature=True)
   parser.add_argument(
       '--maxGames',
       type=int,
       help='Maximum number of games to download',
       default=int(20e3))
+  parser.add_argument(
+      '--game', type=str,
+      help='If set, downloads and prings the game log for this game',
+      default=None)
+  parser.set_defaults(feature=True)
   return parser
 
 
 if __name__ == '__main__':
   parser = setUpParser()
   args = parser.parse_args()
-  text = fetchAllGameSetences(
-      detailsLocal=args.detailsLocal,
-      summaryLocal=args.summaryLocal,
-      saveEvery=args.saveEvery,
-      maxGames=args.maxGames)
-  with open("snellman/games.input", "w") as f:
-    f.write(text)
+  if not args.game:
+    text = fetchAllGameLogs(
+        detailsLocal=args.detailsLocal,
+        summaryLocal=args.summaryLocal,
+        saveEvery=args.saveEvery,
+        maxGames=args.maxGames)
+    with open("snellman/games.input", "w") as f:
+      f.write(text)
+  else:
+    print(downloadLogForGame(Game({
+        'game': args.game
+    }))[-1024:])
